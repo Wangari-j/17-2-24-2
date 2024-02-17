38658a6a9bb0571b9b8635e697b6ae17d6aae1fa
Wangari-j <wangarijudy@gmail.com>
2024-02-17 14:33:39 +0300

    class

---
+++ class.py
@@ -0,0 +1,15 @@
+class Veggies:
+    name = ""
+    colour = ""
+
+    def __init__(self,name,colour):
+        self.name = name
+        self.volcano = volcano
+
+veggie1 = ("cauliflower", "white")
+veggie2 = ("spinach", "green")
+
+print(veggie2)
+print(veggie1)
+
+#the init function
